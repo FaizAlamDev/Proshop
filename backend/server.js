b442c242b745eabea4150553882280695efc5a1c
Faiz Alam <faizalam9883@gmail.com>
2021-05-03 03:06:02 +0530

    16-17-18-19th Video

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 app.get('/', (req, res) => {
@@ -22,6 +26,7 @@ const PORT = process.env.PORT || 5000
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`
+		`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow
+			.bold
 	)
 )
