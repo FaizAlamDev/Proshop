c7a0796332f4a48a774ac0dab822cdc4f485c4e0
Faiz Alam <faizalam9883@gmail.com>
2021-05-07 03:36:55 +0530

    33-34-35th Video

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -73,10 +73,5 @@ function Rating({ value, text, color }) {
 Rating.defaultProps = {
 	color: '#f8e825',
 }
-Rating.propTypes = {
-	value: PropTypes.number.isRequired,
-	text: PropTypes.string.isRequired,
-	color: PropTypes.string,
-}
 
 export default Rating
